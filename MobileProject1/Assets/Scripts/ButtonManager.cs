745c7b58fbd3f7650f65cfc810ca63574ee77106
blastingranger5 <blastingranger5@gmail.com>
2021-02-12 18:10:23 -0500

    Buttons function, used Debug Log to confirm

--- MobileProject1/Assets/Scripts/ButtonManager.cs
+++ MobileProject1/Assets/Scripts/ButtonManager.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ButtonManager : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void RightAnswer()
     {
-        
+        Debug.Log("YESSIREE");
+    }
+
+    public void WrongAnswer()
+    {
+        Debug.Log("*boom*");
     }
 }
